0610a2fda3685673ed6fe1452ba7a0c85cb1c7e9
JonathanCantu <jcantu@jcantu.com>
2026-02-08 22:41:18 -0500

    Week 4 project assignment.

---
+++ week-4/project03-01/project03-01.js
@@ -0,0 +1,32 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-01
+
+      Application to calculate total order cost
+      Author: Jonathan Cantu
+      Date:   February 8, 2026
+
+      Filename: project03-01.js
+*/
+
+let menuItems = document.getElementsByClassName("menuItem");
+
+for (let i = 0; i < menuItems.length; i++) {
+   menuItems[i].addEventListener("click", calcTotal);
+}
+
+// Function to calculate the total cost of the order
+function calcTotal() {
+   let orderTotal = 0;
+   for (let i = 0; i < menuItems.length; i++) {
+      if (menuItems[i].checked) {
+         orderTotal += Number(menuItems[i].value);
+      }
+   }
+   document.getElementById("billTotal").textContent = formatCurrency(orderTotal);
+}
+
+ // Function to display a numeric value as a text string in the format $##.##
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }
---
+++ week-4/project03-01/project03-01_txt.js
@@ -0,0 +1,19 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-01
+
+      Application to calculate total order cost
+      Author: Jonathan Cantu
+      Date:   February 8, 2026
+
+      Filename: project03-01.js
+*/
+
+
+
+
+
+ // Function to display a numeric value as a text string in the format $##.##
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }
